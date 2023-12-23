9345e448a5ad433d8b5e71c442b96ba9b2b19476
jkmathilda <jkmathilda627@gmail.com>
2023-12-22 22:10:09 -0800

    Update main.py Add README.md

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# ChatGPT-Clone
+# ChatGPT
+
+Chatbots are one of the central LLM use-cases. The core features of chatbots are that they can have long-running conversations and have access to information that users want to know about.
+
+Aside from basic prompting and LLMs, memory and retrieval are the core components of a chatbot. Memory allows a chatbot to remember past interactions, and retrieval provides a chatbot with up-to-date, domain-specific information.
+
+
+
+# Getting Started
+
+To get started with this project, you'll need to clone the repository and set up a virtual environment. This will allow you to install the required dependencies without affecting your system-wide Python installation.
+
+### Cloning the Repository
+
+    git clone https://github.com/jkmathilda/ChatGPT-Clone.git
+
+### Setting up a Virtual Environment
+
+    cd ./ChatGPT-Clone
+
+    pyenv versions
+
+    pyenv local 3.11.6
+
+    echo '.env'  >> .gitignore
+    echo '.venv' >> .gitignore
+
+    ls -la
+
+    python -m venv .venv        # create a new virtual environment
+
+    source .venv/bin/activate   # Activate the virtual environment
+
+    python -V                   # Check a python version
+
+### Install the required dependencies
+
+    pip install -r requirements.txt
+
+### Configure the Application
+
+To configure the application, there are a few properties that can be set the environment
+
+    echo 'OPENAI_API_KEY="sk-...."' >> .env
+
+### Running the Application
+
+    python -m streamlit run main.py
+
+### Deactivate the virtual environment
+
+    deactivate
+
+### Reference
+
+Chatbots (on LangChain)
+[https://python.langchain.com/docs/use_cases/chatbots]
+
+Youtube : Create a ChatGPT clone using Streamlit and LangChain
+[https://www.youtube.com/watch?v=IaTiyQ2oYUQ]
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import streamlit as st
-from streamlit_chat import message
-from dotenv import load_dotenv
-import os
+from dotenv import load_dotenv                          # Dotenv for loading environment variables.
+import os                                               # OS module for interacting with the operating system.
+import streamlit as st                                  # Streamlit library for creating web apps.
+from streamlit_chat import message as stchat_message    # Streamlit's chat UI component.
 
+# Import chat-related modules from LangChain.
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (
     SystemMessage,
@@ -11,46 +12,64 @@ from langchain.schema import (
 )
 
 def init():
-    load_dotenv()
-    # Load the OpenAI API key from the environment variable
+    load_dotenv()  # Load environment variables from a .env file.
+
+    # Load the OpenAI API key from an environment variable.
     if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPEN_API_KEY") == "":
-        print("OPeNAI_API_KEY is not set")
+        print(">> OPENAI_API_KEY is not set")
         exit(1)
     else: 
-        print("OPENAI_API_KEY is set")
+        print(">> OPENAI_API_KEY is set")
     
+    # Define Streamlit page configuration.
     st.set_page_config(
-        page_title="Your own ChatGPT",
-        page_icon="⌨️"
+        page_title="Your own ChatGPT",  # Title of the page.
+        page_icon="⌨️"                   # Icon of the page.
     )
 
 def main():
-    init()
+    init()  # Call the initialization function.
     
-    chat = ChatOpenAI(temperature=0.5)
+    # Create an instance of ChatOpenAI from LangChain
+    chat = ChatOpenAI(temperature=0.5)  # Temperature parameter determines the creativity of the conversation.
     
+    # Initialize the Streamlit session state to keep track of the messages.
     if "messages" not in st.session_state:
         st.session_state.messages = [
-            SystemMessage(content="You are a helpful assistant.")
+            SystemMessage(content="You are a helpful assistant.")  # Start the conversation with a system message.
         ]
-        
-    st.header("Your own ChatGPT 🖥️")
     
+    # Set up the header of the page.
+    st.header("Your own ChatGPT.")
+    
+    # Create a text input field in the sidebar for user input.
     with st.sidebar:
         user_input = st.text_input("Your message: ", key="user_input")
-        
-    if user_input:
-        st.session_state.messages.append(HumanMessage(content=user_input))
-        with st.spinner("Thinking..."):
-            response = chat(st.session_state.messages)
-        st.session_state.messages.append(AIMessage(content=response.content))
-        
-    messages = st.session_state.get('messages', [])
-    for i, msg in enumerate(messages[1:]):
-        if i % 2 == 0:
-            message(msg.content, is_user=True, key=str(i) + '_user')
-        else:
-            message(msg.content, is_user=False, key=str(i) + '_ai')
+        show_message = st.radio(
+            "Set a message visibility 👉",
+            key="visibility",
+            options=["visible", "hidden"],
+            index=1,
+        )
     
+    # Process the user's message.
+    if user_input:
+        st.session_state.messages.append(HumanMessage(content=user_input))  # Add the user's message to the session state.
+        with st.spinner("Thinking..."):                                     # Show a spinner while waiting for a response.
+            response = chat(st.session_state.messages)                      # Get a response from the LangChain chatbot.
+        st.session_state.messages.append(AIMessage(content=response.content))  # Add the AI's response to the session state.
+
+    # Iterate through the stored messages and display them on the screen.
+    messages = st.session_state.get('messages', [])
+    
+    for i, msg in enumerate(messages[1:]):                              # Display all messages except the first system message.
+        if i % 2 == 0:
+            stchat_message(msg.content, is_user=True, key=str(i) + '_user')    # Display user messages.
+        else:
+            stchat_message(msg.content, is_user=False, key=str(i) + '_ai')     # Display AI messages.
+
+    if show_message == "visible":
+        st.write(messages)
+
 if __name__ == '__main__':
     main()
